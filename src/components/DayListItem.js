30f27733e396feca40f5ffa73fdad290ac4b4b15
Kineret <kineret.braslavsky@gmail.com>
2021-02-16 20:50:46 -0500

    styling the dayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+import "components/DayListItem.scss";
+const classnames = require('classnames')
+
 export default function DayListItem(props) {
+
+let dayListClass;
+if(props.selected) dayListClass = classnames(`day-list__item--selected`)
+else dayListClass = classnames(`day-list__item`)
+if(props.spots === 0) dayListClass = classnames(`day-list__item--full`)
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayListClass} onClick={()=>{props.setDay(props.name)}}  data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
