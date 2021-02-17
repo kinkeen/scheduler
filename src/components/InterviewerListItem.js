c218be45c86ffb1244e57008f165de25dc8a3bcb
Kineret <kineret.braslavsky@gmail.com>
2021-02-17 15:44:37 -0500

    building the interviewerList component and added styling

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,7 +3,7 @@ import React from "react";
 import "components/InterviewerListItem.scss";
 const classNames = require('classnames')
 
-export default function DayListItem(props) {
+export default function InterviewerListItem(props) {
 
     let interviewerClass;
     props.selected? interviewerClass = classNames(`interviewers__item--selected`) : interviewerClass = classNames(`interviewers__item`)
