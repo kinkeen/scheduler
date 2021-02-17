d1120ad473d6d2125c875daef9b8a5701bec0220
Kineret <kineret.braslavsky@gmail.com>
2021-02-17 18:48:53 -0500

    adding state to the appointment Form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
   //-------------Button Story--------------------------------/
 storiesOf("Button", module)
@@ -191,3 +192,19 @@ storiesOf("Button", module)
             onClose={action("onClose")}
           />
         )) 
+        .add("Edit", () => (
+          <Form 
+            name={"Default Student"}
+            interviewers={interviewers}
+            interviewer={interviewer.id}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
+          />
+        ))
+        .add("Create", () => (
+          <Form 
+            interviewers={interviewers}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
+          />
+        ))
