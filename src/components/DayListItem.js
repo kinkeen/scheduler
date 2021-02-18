333a96ee013739f7ddfe87c0f956e497c0817c6d
Kineret <kineret.braslavsky@gmail.com>
2021-02-17 22:14:23 -0500

    fixsing bug the related to the calssName

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,21 +4,31 @@ import "components/DayListItem.scss";
 const classnames = require('classnames')
 
 export default function DayListItem(props) {
-    const formatSpots = function(spotsLeft) {
-        if (spotsLeft === 0) return "no spots remaining";
-        if (spotsLeft  < 5)  return `${spotsLeft} spot${spotsLeft>1? "s":""} remaining`;
-        return spotsLeft;
-    }
 
-    let dayListClass;
-    if(props.spots === 0) dayListClass = classnames(`day-list__item--full`)
-    if(props.selected) dayListClass = classnames(`day-list__item--selected`)
-    else dayListClass = classnames(`day-list__item`)
+    const basicClass = "day-list__item";
+    const selectedClass = "day-list__item--selected";
+    const fullClass = "day-list__item--full";
+    let dayListClass = basicClass;
+    let dayListText = "";
     
+    if(props.selected){
+        dayListClass += ` ${selectedClass}`
+    } 
+    
+    let spotsLeft = props.spots;
+
+    if (spotsLeft === 0){
+        dayListText = "no spots remaining";
+        dayListClass += ` ${fullClass}` 
+    }else if (spotsLeft  < 5) { 
+    
+        dayListText  = `${spotsLeft} spot${spotsLeft>1? "s":""} remaining`;
+    } 
+
     return (
         <li className={dayListClass} onClick={()=>{props.setDay(props.name)}}  data-testid="day">
             <h2 className="text--regular">{props.name}</h2>
-            <h3 className="text--light">{formatSpots(props.spots)}</h3>
+            <h3 className="text--light">{dayListText}</h3>
         </li>
     );
 }
