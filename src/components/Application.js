eeaa77a3a1bd5516848582ac6cbc94adf1b650be
Kineret <kineret.braslavsky@gmail.com>
2021-02-18 07:39:17 -0500

    adding the scheduler to the application

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,50 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }, 
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Miller Choen",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Lydia Joneson",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
+];
 
 const days = [
   {
@@ -23,6 +67,11 @@ const days = [
 
 export default function Application(props) {
   const[day,setDay] = useState("Monday");
+  const schedulesData = appointments.map((appointment,index) => {
+    return( 
+      <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />
+    )
+  })
   
   return (
     <main className="layout">
@@ -38,6 +87,7 @@ export default function Application(props) {
           days={days}
           day={day}
           setDay={setDay}
+          setDay={day => setDay(day)}
         />
         </nav>
         <img
@@ -48,6 +98,7 @@ export default function Application(props) {
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
+          {schedulesData}
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
