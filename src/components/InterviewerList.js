6beb773d28cd0c7fae1eaa92ac33fdb1431b5769
Kineret <kineret.braslavsky@gmail.com>
2021-02-24 17:55:05 -0500

    working on tests of appointment & form componnents

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,33 +1,34 @@
 
 import React from "react";
 import "components/InterviewerList.scss";
-import InterviewerListItem from "./InterviewerListItem";
+import InterviewerListItem from "components/InterviewerListItem";
 import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
-    const interviewersData = props.interviewers.map((interviewer) => {
+
+    const interviewersData = props.interviewers.map((interviewer) => {    
         return (
-            <InterviewerListItem
-         
-                key={interviewer.id}
-                name={interviewer.name}
-                avatar={interviewer.avatar}
-                selected={props.interviewer === interviewer.id}
-                setInterviewer={(event) => props.setInterviewer(interviewer.id)}
+            <InterviewerListItem 
+                key = {interviewer.id}
+                name = {interviewer.name}
+                avatar = {interviewer.avatar}
+                selected = {interviewer.id === props.value}
+                setInterviewer={() => props.onChange(interviewer.id)}
             />
         )
     })
+
     return (
         <section className="interviewers">
-            <h4 className="interviewers__header text--light">Interviewer</h4>
-            <ul className="interviewers__list">
-                { interviewersData }
-            </ul>
+        <h4 className="interviewers__header text--light">Interviewer</h4>
+        <ul className="interviewers__list">
+            {interviewersData}
+        </ul>
         </section>
     )
-} 
+}
   
 InterviewerList.propTypes = {
     value: PropTypes.number,
     onChange: PropTypes.func.isRequired
-};
+  };
