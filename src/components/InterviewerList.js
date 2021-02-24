a3e12e66e45065a8e82a032c01857269a70a9858
Kineret <kineret.braslavsky@gmail.com>
2021-02-24 14:34:40 -0500

    type Checking with Prop types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,25 +1,33 @@
 
 import React from "react";
 import "components/InterviewerList.scss";
-import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
-
+    const interviewersData = props.interviewers.map((interviewer) => {
+        return (
+            <InterviewerListItem
+         
+                key={interviewer.id}
+                name={interviewer.name}
+                avatar={interviewer.avatar}
+                selected={props.interviewer === interviewer.id}
+                setInterviewer={(event) => props.setInterviewer(interviewer.id)}
+            />
+        )
+    })
     return (
         <section className="interviewers">
-        <h4 className="interviewers__header text--light">Interviewer</h4>
-        <ul className="interviewers__list">
-            {props.interviewers.map(interviewer =>
-            <InterviewerListItem 
-                key = {interviewer.id}
-                name = {interviewer.name}
-                avatar = {interviewer.avatar}
-                selected = {interviewer.id === props.value}
-                setInterviewer={() => props.onChange(interviewer.id)}
-            />
-            )}
-        </ul>
+            <h4 className="interviewers__header text--light">Interviewer</h4>
+            <ul className="interviewers__list">
+                { interviewersData }
+            </ul>
         </section>
-    );
-}
+    )
+} 
   
+InterviewerList.propTypes = {
+    value: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+};
